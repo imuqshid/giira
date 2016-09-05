be7c742f267edf329ec0fbba4486f46a910ad488
DESKTOP-MUQSHID\Muqshid <muqshidm@gmail.com>
2016-09-05 21:33:35 +0530

    Changed th app name to Giira

--- app/src/main/java/gira/cdap/com/giira/MainActivity.java
+++ app/src/main/java/gira/cdap/com/giira/MainActivity.java
@@ -1,22 +1,32 @@
 package gira.cdap.com.giira;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.Filterable;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 import gira.cdap.com.giira.activity.LoginActivity;
 import gira.cdap.com.giira.helper.SQLiteHandler;
 import gira.cdap.com.giira.helper.SessionManager;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener{
     private Boolean isFabOpen = false;
     private FloatingActionButton fab;
     private ImageButton btnLinknear;
@@ -30,6 +40,9 @@ public class MainActivity extends AppCompatActivity {
     ImageButton eventIcon;
     ImageButton disasterIcon;
     ImageButton profileIcon;
+
+    AutoCompleteTextView autoCompleteTextView;
+    private RecyclerView mRecyclerView,mRecyclerViewRegion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -103,6 +116,42 @@ public class MainActivity extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+    public void initalize()
+    {
+        autoCompleteTextView = (AutoCompleteTextView) findViewById(R.id.autoCompletesearch);
+        autoCompleteTextView.setAdapter(new GooglePlacesAutocompleteAdapter(this,   R.layout.list_user_card, R.id.results_user));
+        autoCompleteTextView.setOnItemClickListener(this);
+
+
+    }
+
+    @Override
+    public void onItemClick(AdapterView adapterView, View view, int position, long id) {
+
+        Intent intent=new Intent(this,one_place.class);
+        intent.putExtra("id", id);
+        intent.putExtra("name", username);
+
+        startActivity(intent);
+
+    }
+
+    class GooglePlacesAutocompleteAdapter extends ArrayAdapter implements Filterable {
+        private ArrayList resultList;
+
+//        public GooglePlacesAutocompleteAdapter(Context context, int textViewResourceId, int textView1) {
+//            super(context,textView1);
+//        }
+
+        public GooglePlacesAutocompleteAdapter(Context context, int resource,
+                                               int textViewResourceId) {
+
+            super(context, resource, textViewResourceId);
+        }
+
+
     }
     private void logoutUser() {
         session.setLogin(false);
